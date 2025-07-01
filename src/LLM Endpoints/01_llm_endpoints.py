72969c643fcf474bdaa3081c5436c3b2fac0dc5b
Khanh Nguyen <19ngxuan@gmail.com>
2025-07-01 12:11:01 +0200

    Add LLM endpoints

---
+++ src/LLM Endpoints/01_llm_endpoints.py
@@ -0,0 +1,53 @@
+def prompt_ollama(prompt:str, model="codellama:7b"):
+    """A prompt helper function that sends a prompt to 
+    ollama and returns only the text response."""
+    import openai
+    # setup connection to the LLM server
+    client = openai.OpenAI(
+        base_url = "http://localhost:11434/v1",
+        api_key = "none" # not required by ollama
+    )
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    
+    # extract answer
+    return response.choices[0].message.content
+
+def prompt_blablador(prompt:str, model="alias-llama3-huge"):
+    """A prompt helper function that sends a prompt to Blablador (FZ Jülich)
+    and returns only the text response.
+    """
+    import os
+    
+    # setup connection to the LLM-server
+    client = openai.OpenAI(
+        base_url = "https://helmholtz-blablador.fz-juelich.de:8000/v1",
+        api_key = os.environ.get('BLABLADOR_API_KEY')
+    )
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    
+    # extract answer
+    return response.choices[0].message.content
+
+def prompt_kisski(prompt:str, model="meta-llama-3.1-70b-instruct"):
+    """A prompt helper function that sends a message to KISSKI Chat AI API
+    and returns only the text response.
+    """
+    # setup connection to the LLM-server
+    client = openai.OpenAI(
+        base_url="https://chat-ai.academiccloud.de/v1",
+        api_key=os.environ.get('KISSKI_API_KEY')
+    )
+    
+    response = client.chat.completions.create(
+        model=model,
+        messages= [{"role": "user", "content": prompt}]
+    )
+    
+    # extract answer
+    return response.choices[0].message.content
