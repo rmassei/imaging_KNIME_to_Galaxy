1c5634d8b0efd34aecc94088e95aa635cde2b1d5
Khanh Nguyen <19ngxuan@gmail.com>
2025-09-16 11:07:03 +0200

    [REFACTOR] Delete files unrelated to jupyter notebook

--- src/java2py/converter/translator.py
+++
@@ -1,8 +0,0 @@
-# converter/translator.py
-from .llm_endpoints import prompt_scadsai_llm
-from .prompt_builder import build_prompt
-
-class JavaToPythonTranslator:
-    def translate_code(self, java_code: str) -> str:
-        prompt = build_prompt(java_code)
-        return prompt_scadsai_llm(prompt)
--- src/main.py
+++
@@ -1,7 +0,0 @@
-from src import parser_processors
-
-# Process all workflows in the base directory
-parser_processors.process_all_workflows("test_data")
-
-
-
