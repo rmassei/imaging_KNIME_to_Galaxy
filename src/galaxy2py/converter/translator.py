ba2a11ed5768033b96a835ce8338ab239c3d2703
Khanh Nguyen <19ngxuan@gmail.com>
2025-11-06 09:29:19 +0100

    [REFACTOR] - Delete files

--- src/galaxy2py/converter/translator.py
+++
@@ -1,8 +0,0 @@
-# converter/translator.py
-from .llm_endpoints import prompt_scadsai_llm
-from .prompt_builder import build_prompt
-
-class GalaxyToPythonTranslator:
-    def translate_code(self, json_str: str) -> str:
-        prompt = build_prompt(json_str)
-        return prompt_scadsai_llm(prompt)
--- src/knime2galaxy/llm/translator.py
+++
@@ -1,8 +0,0 @@
-# llm/translator.py
-from .llm_endpoints import prompt_scadsai_llm
-from .prompt_builder import build_prompt
-
-class Knime_to_Galaxy_Translator:
-    def translate_code(self, source: str) -> str:
-        prompt = build_prompt(source)
-        return prompt_scadsai_llm(prompt)
