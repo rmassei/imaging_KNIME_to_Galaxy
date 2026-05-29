8bd3eaf814c898a6dc3317a96157c7994da84b3a
lea-33 <lg77sypy@studserv.uni-leipzig.de>
2026-05-29 10:47:04 +0200

    [refactor] update save_answer_to_file to optionally create a new output folder

--- src/imaging_knime_to_galaxy/knime_io.py
+++ src/imaging_knime_to_galaxy/knime_io.py
@@ -70,6 +70,6 @@ def replace_uuid(json_object):
     return json_object
 
 def save_answer_to_file(json_object, output_path):
-    output_file = output_path
-    with open(output_file, "w", encoding="utf-8") as f:
-      json.dump(json_object, f, indent=2, ensure_ascii=False)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, "w", encoding="utf-8") as f:
+    json.dump(json_object, f, indent=2, ensure_ascii=False)
